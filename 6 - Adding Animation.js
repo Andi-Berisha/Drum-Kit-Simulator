15316bc0b0e2a56d8261a23962b93c418fbca6a6
Andi Berisha <aberisha@qa.com>
2021-07-07 00:40:47 +0100

    annotations to functions

--- 6 - Adding Animation.js
+++ 6 - Adding Animation.js
@@ -13,9 +13,9 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
 
 // this adds an event listener to the whole document
 document.addEventListener('keydown', function (event) {
-  makeSound(event.key);
+  makeSound(event.key); //runs the switchcase in the makesound function where the switch(expression) is equal to switch(event.key)
 
-  buttonAnimation(event.key);
+  buttonAnimation(event.key); //passes whatever key was pressed to the buttonAnimation function
 
   //same thing as above except the sound is placed depending on the button that is clicked
 });
@@ -65,14 +65,15 @@ function makeSound(button) {
 }
 
 function buttonAnimation(currentKey) {
-  //here we are pointing toward the class the the button exists in
+  //here we are pointing toward the element that the styling for the button exists in
   //by concatinating a class selector "." with the key that was pressed
 
-  var activeButton = document.querySelector('.' + currentKey);
-  activeButton.classList.toggle('pressed');
+  var activeButton = document.querySelector('.' + currentKey); //  if currentKey = k then this line of code represents class ="k drum"
+  activeButton.classList.add('pressed'); //here we attach the '.pressed' class to the button element with the class = k"drum"
 
   // the setTimeout function takes 2 parameters; the first is the function you want to execute
   // and the second is the number of ms to wait before executing the code.
+  // so here we are essentially removing the pressed css from the element after 100ms to create a pressed animation.
   setTimeout(function () {
     activeButton.classList.remove('pressed');
   }, 100);
